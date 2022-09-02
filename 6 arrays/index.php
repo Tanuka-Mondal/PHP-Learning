478d2e6eb61a3d2f8c05b060ca8b711c71918876
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-09-02 13:54:05 +0530

    Create index.php

---
+++ 6 arrays/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="container">
+       
+        <?php
+           $colour = array("red", "green", "blue", "yellow","pink");
+           echo $colour[0]; // red
+           echo "<br>";
+           echo $colour[1]; // green
+           echo "<br>";
+           echo $colour[2]; // blue
+           echo "<br>";
+           echo $colour[3]; // yellow
+           echo "<br>";
+           echo $colour[4]; // pink
+           echo "<br>";
+           echo count($colour); // 5
+
+
+        ?>
+    </div>
+</body>
+</html>
