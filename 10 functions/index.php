7723fd7ef04cab699e790b25acd94775af7aa979
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-09-08 18:07:18 +0530

    Create index.php

---
+++ 10 functions/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="container">
+       
+        <?php
+           function sum ($a, $b) {
+            echo "<br>";
+               return $a + $b;
+           }
+              echo sum(2, 3);
+              echo sum(4, 5);
+                           
+        ?>
+    </div>
+</body>
+</html>
